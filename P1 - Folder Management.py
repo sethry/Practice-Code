ba205e6da71c4b2e322e39369c7b4c2701ad0a3a
AY <alekseipetrovitch@gmail.com>
2018-04-01 16:58:42 -0600

    Preliminary Code
    Added basic code from the internet that needs to be adapted to our specific problem.

--- P1 - Folder Management.py
+++ P1 - Folder Management.py
@@ -1,1 +1,19 @@
 # This file holds practice code for PYTHON
+
+import os  # required to manipulate 
+
+def createFolder(directory):
+
+    try:
+
+        if not os.path.exists(directory):
+
+            os.makedirs(directory)
+
+    except OSError:
+
+        print ('Error: Creating directory. ' +  directory)
+
+
+createFolder('./data/')
+# Creates a folder in the current directory called data
