ab13341539e3f10efde593a0ee4452075c265d0c
AY <alekseipetrovitch@gmail.com>
2018-04-02 14:49:51 -0600

    Setup
    Added descriptions of a for-loop and preliminary name lists that we can use to create folders.

--- P1 - Folder Management.py
+++ P1 - Folder Management.py
@@ -1,4 +1,25 @@
-# This file holds practice code for PYTHON
+# This file holds practice code for PYTHON.
+
+# The purpose of this code is to create a lot of directories. Say we get a list
+# of a hundred names in a Word document, how can you create individual folders
+# for all these people in a certain place?
+
+# Solution: We use a "for loop". This is something in programming that does
+# something over and over again. Example:
+
+for i in [5, 4, 2, 8, -1, -2]:  # for each thing in the brackets...
+    print(i)
+
+# This code prints all the numbers in the brackets one by one. It does 'print(i)'
+# over and over again.
+
+# We can also write
+
+for i in range(5):
+    print(i)
+
+# This is different because we use 'range(5)'. This creates a bracketed list...
+# [0, 1, 2, 3, 4] of ascending numbers. It's useful for iterating.
 
 import os  # required to manipulate 
 
